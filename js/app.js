38d2fcc3d8bf1c4ca37b9fb6be629383f9861e6b
yolixtly <yolixtly_13@hotmail.com>
2016-03-12 16:44:54 -0500

    App working

--- js/app.js
+++ js/app.js
@@ -27,22 +27,38 @@ console.log("Spices of the World Quiz");
 			/*question index currently Displayed*/
 			currentQuestion,
 			/*Buttons of Quiz-Section*/
+			buttonArea,
 			reStartButton,
 			hintButton,
-			nextQButton;
+			nextQButton,
+			/*Processing information*/
+/*Mehran ?*/userAnswer, //declared in line 84
+			userAnswerArray,
+			currentCorrectAnswer,
+			/*Others*/
+			feedbackArea,
+			finalScore,
+			score, //for percentage purposes
+			complement;
 
-/*---------------------------------------------------------------------------------
-   Declaring variables with Jquery values to fetch html elements
----------------------------------------------------------------------------------*/
+
+$(document).ready(function(){
+
+
+			/*---------------------------------------------------------------------------------
+			   DOM SELECTION
+			   Declaring variables with Jquery values to fetch html elements
+			---------------------------------------------------------------------------------*/
 			/*Page section values*/
 			gameSection = $('#quizApp');
 			resultSection = $('#score-page');
 			welcomePage = $('#init-page');
 
-			/*Init Page Elements*/
+			/*Init Page Elements for Effect purposes*/
 			bubbles = $('#bubbles');
+			// icons = bubbles.find('.img-logo');
 
-			/*Display areas values for Questions, Answers & Hints*/
+			/*Display area values for: Questions, Answers & Hints*/
 			answersArea = $('#answers-area'); //div id='answers-area' wrapping all the spans; line 44 index.html
 			
 				question = $('#question-area');
@@ -55,107 +71,90 @@ console.log("Spices of the World Quiz");
 				hintContent = $('#hint-content');
 
 			/*Buttons values*/
+			buttonArea = $('#button-area'); //contains restart/hint/nextQbutton
 			startButton = $('#start-button');
 			reStartButton = $('#restart');
 			hintButton =$('#hint');
 			nextQButton = $('#nextQuestion');
 			
-			// icons = bubbles.find('.img-logo');
+
+			/*Display of Score*/
+			feedbackArea = $('.feedback-area');
+			finalScore =$('#score-text');
+			complement =$('#complement');
+
+			/*Processing information*/
+
+			userAnswer = $("input[type='radio']:checked").val(); 
+			userAnswerArray = []; //elements are pushed where 1 is correct and 0 is wrong answer eg. [0,1,1,1,0]
 
 			/*---------------------------------------------------------------------------------
     			END of Declaring variables 
 			---------------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------------
-    Initial Page 
----------------------------------------------------------------------------------*/
 
-$(document).ready(pageLoad);
 
-function pageLoad(){
-
-	/*What to show and what to hide*/
+	/*What to show and what to hide when the page Loads*/
 
 		//Animation and Effects of Elements
-		startEffects();
+			startEffects();
 
-		//Hide Other Sections of the App
-		gameSection.hide();
-		resultSection.hide();
+		//Hide Other Sections of the App when page uploads
+			gameSection.hide();
+			resultSection.hide();
+
+	// New Game Loads With Page
+			newGame();
+
+
+		/*------Start Game Function------*/
+
+	function newGame(){
+
+		startButton.click(function(){
+			/*change Pages*/
+			welcomePage.hide();
+			gameSection.show();
+
+			/*Initial Count value of Global Variable*/
+
+			currentQuestion = 0;
 		
-	// New Game Load With Page
-		newGame();
+			/*Display of First Question Group given the above variable*/
+			generateQuestionGroup(currentQuestion);
 
-	
+				/*Re Start Game Button*/
+				reStart()
+				/*hint Button content show/hide function*/
+				hintDisplay();
+				/*Next Question button*/
+				nextAnswer();
 
-};
+				/*Hidden hint*/
+				hintContent.hide();
 
+		});
+	};
 
+	/*---------------------------------------------------------------------------------
+	    Question Object (DataBase)
+	---------------------------------------------------------------------------------*/
 
-
-
-	/*------Start Button Effects------*/
-
-function startEffects(){
-
-	startButton.mouseenter(function(){
-		startButton.addClass('game-start');
-		startButton.removeClass('title');
-		startButton.text('Start Game!');
-	})
-	startButton.mouseleave(function(){
-		startButton.addClass('title');
-		startButton.removeClass('game-start');
-		startButton.text('Spices of the World');
-	})
-};
-
-	/*------Start Game Function------*/
-
-function newGame(){
-
-	startButton.click(function(){
-		/*change Pages*/
-		welcomePage.hide();
-		gameSection.show();
-
-		/*Hiding hint*/
-		hintContent.hide();
-
-		/*Initial values of questions and score*/
-
-		currentQuestion = 0;
-
-		/*Display of Question Groups*/
-		generateQuestionGroup(currentQuestion);
-
-		/*Re Start Game Button*/
-		reStart()
-		/*Next Question button*/
-		nextAnswer();
-		/*hint content show/hide function*/
-		hintDisplay();
-	});
-};
-
-/*---------------------------------------------------------------------------------
-    Question Object (Data Base)
----------------------------------------------------------------------------------*/
-
-function Question(question, arrayOptions,CorrectIndex, hint){
-	this.question = question;
-	this.option = arrayOptions;
-	this.correctAnswer = arrayOptions[CorrectIndex];
-	this.hint = hint;
-};
+	function Question(question, arrayOptions,CorrectIndex, hint){
+		this.question = question;
+		this.option = arrayOptions;
+		this.correctAnswer = CorrectIndex;
+		// this.correctAnswer = arrayOptions[CorrectIndex]; // Requires extra code to evaluate 
+		this.hint = hint;
+	};
 
 	/*Objects of 'Question'*/
 
-	questionGroup = new Array();
+	questionGroup = new Array(); //Array of Objects
 	questionGroup[0] = new Question(
 		//question
 		"Which spice is known for it’s versatility usage in savory and sweet foods?",
 		//arrayOptions
-		['Cinnamon','Paprika','Rosemery','Vanilla','Wasabi'], 
+		['Cinnamon','Paprika','Rosemary','Vanilla','Wasabi'], 
 		//CorrectIndex
 		0,
 		//hint
@@ -186,9 +185,9 @@ function Question(question, arrayOptions,CorrectIndex, hint){
 		'It is the second most-used spice in the U.S., following peppercorns.'
 		);
 
-/*---------------------------------------------------------------------------------
-    Functions 
----------------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------------
+	    Functions 
+	---------------------------------------------------------------------------------*/
 
 	/*Function to Render the Question[currentQuestion] values in the DOM*/
 
@@ -204,53 +203,139 @@ function Question(question, arrayOptions,CorrectIndex, hint){
 		hintContent.text(questionGroup[currentQuestion].hint);
 	};
 
-			/*Hint display Function*/
+		/*Function to Restart the Game with Restart button*/
 
+	function reStart(){
+		reStartButton.click(function(){
+			//Reseting Values 
+			currentQuestion = 0;
+			userAnswerArray = [];
+			feedbackArea.text('');
+			//Render the first object of the Array
+			generateQuestionGroup(currentQuestion);
+
+		});
+
+	};
+
+			/*Hint display Button Function*/
 
 	function hintDisplay(){
-		hintButton.click(function(){
-			hintContent.toggle('slow');
+		hintButton.mouseenter(function(){
+			hintContent.show();
+		});
+		hintButton.mouseleave(function(){
+			hintContent.hide();
 		});
 	};
 
-	// function hintDisplay(){
-	// 	hintButton.mouseenter(function(){
-	// 		hintContent.show();
-	// 	});
-	// 	hintButton.mouseleave(function(){
-	// 		hintContent.hide();
-	// 	});
-	// };
 
-	console.log('smile!');
+	/*Function to Sum all the userAnswers
+	(elements) from the userAnswerArray*/
+	 function addUpScore(questionGroup){
+	 	return userAnswerArray.reduce(function(previousValue, currentValue, currentIndex, array){
+	 		return previousValue + currentValue;
+	 	});
+	 }
 
-	/*nextQuestion button Function*/
+	 /*Function to Evaluate user 
+	 Answer Right or Wrong*/
+	 function answerEvaluation(){
+	 	if($("input[type='radio']:checked").val() == questionGroup[currentQuestion -1].correctAnswer){
+	 		userAnswerArray.push(1); //this pushes userAnswer to the userAnswerArray
+	 		feedbackArea.text("You're score is: " + addUpScore(questionGroup) + " correct out of " + questionGroup.length + ".");
+	 	} else {
+	 		userAnswerArray.push(0); //
+	 		feedbackArea.text("You're score is: " + addUpScore(questionGroup) + " correct out of " + questionGroup.length + ".");
+	 	}
+	 };
 
-	// function nextAnswer(){
-	// 	nextQButton.on('click', function(){
-	// 		if(currentQuestion < questionGroup.length -1){
-	// 			currentQuestion++;
-	// 		}
-	// 		generateQuestionGroup(currentQuestion);
-	// 	})
-	// };
 
+	/*nextQuestion button Function to iterate the array
+	of questions and render its content into the webpage*/
 
 	function nextAnswer(){
 		nextQButton.click(function(){
 			if(currentQuestion < questionGroup.length -1) {
 				currentQuestion++;
+				answerEvaluation();
+				generateQuestionGroup(currentQuestion);
+			} else {
+				endGame();
 			}
-		generateQuestionGroup(currentQuestion);
 		});
 	};
 
-	/*Function to iterate over the question objects when clicking on the submit button*/
+										/*	function nextAnswer(){
+											buttonArea.on('click', 'nextQButton', function(){
+												if(currentQuestion < questionGroup.length -1){
+													currentQuestion++;
+												};
+											})
+												generateQuestionGroup(currentQuestion);
+										};*/
 
-	function reStart(){
-		reStartButton.click(function(){
-			currentQuestion = 0;
-			generateQuestionGroup(currentQuestion);
-		});
+	function endGame(){
 
+				currentQuestion++;
+				answerEvaluation();
+				gameSection.hide();
+				resultSection.show();
+				startEffects();
+
+				
+				/*Switch statement to Render the 
+				final Score and the Final feedback*/
+
+		score = ((addUpScore(questionGroup) / questionGroup.length) * 100);
+		
+		switch(addUpScore(questionGroup)){
+			case 0:
+				finalScore.text("Your final Score is " + score + "%");
+				complement.text("Cooking is a Human Thing, are you a human?!");
+				break;
+			case 1:
+				finalScore.text("Your final Score is " + score + "%");
+				complement.text("Cooking is part of Evolution, make it yours!");
+				break;
+			case 2:
+				finalScore.text("Your final Score is " + score + "%");
+				complement.text("Wow I see you cook a little, Way to GO!!");
+				break;
+			case 3:
+				finalScore.text("Your final Score is "+ score + "%");
+				complement.text("Mastering Culinary arts is possible,<br/> You can do it!!");
+				break;
+			case 4:
+				finalScore.text("Your final Score is " + score + "%");
+				complement.text("One day, you will be a Master Chef");
+				break;
+			case 5:
+				finalScore.text("Your final Score is " + score + "%");
+				complement.text("You are a Master Chef");
+				break;
+			default:
+				break;
+		};
 	};
+
+
+
+
+/*------Start Button Effects------*/
+
+	function startEffects(){
+
+		startButton.mouseenter(function(){
+			startButton.addClass('game-start');
+			startButton.removeClass('title');
+			startButton.text('Start Game!');
+		})
+		startButton.mouseleave(function(){
+			startButton.addClass('title');
+			startButton.removeClass('game-start');
+			startButton.text('Spices of the World');
+		})
+	};
+
+});
